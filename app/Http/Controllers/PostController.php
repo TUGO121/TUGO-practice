3f21f2e040322e2764bf71ada848b80790b356a4
TUGO121 <shengdouhe@gmail.com>
2023-03-27 16:37:18 +0900

    ブログ本文作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,9 @@ class PostController extends Controller
        
     
     }
+    public function show(Post $post)
+    {
+         return view('posts/show')->with(['post' => $post]);
+        // dd($post);
+    }
 }
